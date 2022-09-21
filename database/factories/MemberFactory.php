1895e7b15cc5e61c91bad2e59377f6ea6816a573
Mohd Shafiq Izwan B Abdullah <shfqizwn@outlook.my>
2022-09-21 23:51:06 +0800

    first commit

---
+++ database/factories/MemberFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Member>
+ */
+class MemberFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'firstname'=>fake()->sentence(1, false),
+            'lastname'=>fake()->sentence(1, false)
+        ];
+    }
+}
---
+++ resources/views/show.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+
+@section('content')
+<h1 class="page-header text-center">Laravel Jquery Ajax CRUD Bootstrap 5 Modal</h1>
+@endsection
