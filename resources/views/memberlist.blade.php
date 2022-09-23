070de29c67e03c74a6bacc5dd64cb727e5f4e0be
Mohd Shafiq Izwan B Abdullah <shfqizwn@outlook.my>
2022-09-23 08:32:51 +0800

    continue check modal

--- resources/views/memberlist.blade.php
+++ resources/views/memberlist.blade.php
@@ -3,7 +3,7 @@
         <td>{{ $member->firstname }}</td>
         <td>{{ $member->lastname }}</td>
         <td>
-            <a href="#" class="btn btn-success edit" data-id="{{ $member->id }}"> Edit</a>
+            <a href="#" class="btn btn-success edit" data-id="{{ $member->id }}" data-first='{{ $member->firstname }}' data-last='{{ $member->lastname }}'> Edit</a>
             <a href="#" class="btn btn-danger delete" data-id="{{ $member->id }}"> Delete</a>
         </td>
     </tr>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
     <div class="col-md-10 col-md-offset-1">
         <h2>Members Table
-            <button type="button" id="add" data-bs-toggle="modal" data-bs-target="#addnew" class="btn btn-primary pull-right">Member</button>
+            <button type="button" id="add" data-bs-toggle="modal" data-bs-target="#addnew" class="btn btn-primary pull-right">Add Member</button>
         </h2>
     </div>
 </div>
@@ -30,7 +30,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="myModalLabel">Add New Member</h5>
+                <h5 class="modal-title" id="myModalLabel"></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
 
@@ -56,6 +56,40 @@
         </div>
     </div>
 </div>
+{{-- modal end --}}
+
+{{-- edit modal --}}
+{{-- <div class="modal fade" id="editmodal" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="myModalLabel"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+                <form action="{{ URL::to('save') }}" id="addForm">
+                    <div class="mb-3">
+                        <label for="firstname">First Name:</label>
+                        <input type="text" id="firstname" name="firstname" class="form-control" placeholder="Input First Name" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="firstname">Last Name:</label>
+                        <input type="text" id="lastname" name="lastname" class="form-control" placeholder="Input Last Name" required>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div> --}}
+{{-- edit modal end --}}
 
 @endsection
 
@@ -63,23 +97,15 @@
 <script type="text/javascript">
 $(document).ready(function () {
 
-    //ajax setup
-    // $.ajaxSetup(
-    //     {
-    //         headers: {
-    //             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    //         }
-    //     }
-    // );
-
     showMember();
 
     // click submit btn
     $('#addForm').on('submit', function (e) {
         e.preventDefault();
+        // $('#myModalLabel').html("Add New Record");
         var form = $(this).serialize();
         var url = $(this).attr('action');
-        var fn = $(this).attr
+        // var fn = $(this).attr
 
         $.ajax({
             type: 'POST',
@@ -98,27 +124,37 @@ $(document).ready(function () {
         });
     });
 
-    // $('#addForm').on('submit', function(e){
-    //             e.preventDefault();
-    //             var form = $(this).serialize();
-    //             var url = $(this).attr('action');
-    //             $.ajax({
-    //                 type: 'POST',
-    //                 url: url,
-    //                 data: form,
-    //                 dataType: 'json',
-    //                 success: function(){
-    //                     $('#addnew').modal('hide');
-    //                     $('#addForm')[0].reset();
-    //                     showMember();
-    //                 }
-    //             });
-    //         });
+    // edit record
+    $(document).on('click', '.edit', function(event) {
+        event.preventDefault();
+        $('#myModalLabel').html("Edit Record");
+        // alert('button edited clicked' + );
+        var id = $(this).data('id');
+        var firstname = $(this).data('first');
+        var lastname = $(this).data('last');
+        
+        // $('#editmodal').modal('show');
+        $('#addnew').modal('show');
+        
+        $('#firstname').val(firstname);
+        $('#lastname').val(lastname);
+        $('#memid').val(id);
 
+    });
+
+    // $(document).on('click', '.edit', function(event){
+    //             event.preventDefault();
+    //             var id = $(this).data('id');
+    //             var firstname = $(this).data('first');
+    //             var lastname = $(this).data('last');
+    //             $('#editmodal').modal('show');
+    //             $('#firstname').val(firstname);
+    //             $('#lastname').val(lastname);
+    //             $('#memid').val(id);
+    //         });
 
     function showMember()
     {
-        // alert("list all records");
         $.get("{{ URL::to('show') }}", function(data) {
             $('#memberBody').empty().html(data);
         })
